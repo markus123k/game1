968fd9d7a517d7c3ba97e36191e75b005c375121
markus123K/game1 <Eerik79812@gmail.com>
2018-11-30 16:12:32 +0200

    HTML

--- kalkolaator.js
+++ kalkolaator.js
@@ -1,23 +1,23 @@
 document.getElementById("mark").onclick=function() {
-	let number1=parseInt(document.getElementsByTagName("input")[0].value);
-	let number2=parseInt(document.getElementsByTagName("input")[1].value);
-	let number3=parseInt(number1 + number2);
+	let number1=parseFloat(document.getElementsByTagName("input")[0].value);
+	let number2=parseFloat(document.getElementsByTagName("input")[1].value);
+	let number3=parseFloat(number1 + number2);
 	(number3);
 	document.getElementsByTagName("span")[0].innerHTML=number3;
 }
 
 document.getElementById("piip").onclick=function(){
-	let number1=parseInt(document.getElementsByTagName("input")[0].value);
-	let number2=parseInt(document.getElementsByTagName("input")[1].value);
-	let number3=parseInt(number1*number2);
+	let number1=parseFloat(document.getElementsByTagName("input")[0].value);
+	let number2=parseFloat(document.getElementsByTagName("input")[1].value);
+	let number3=parseFloat(number1*number2);
 	(number3);
 	document.getElementsByTagName("span")[0].innerHTML=number3;
 }
 
 document.getElementById("toot").onclick=function() {
-	let number1=parseInt(document.getElementsByTagName("input")[0].value);
-	let number2=parseInt(document.getElementsByTagName("input")[1].value);
-	let number3=parseInt(number1/number2);
+	let number1=parseFloat(document.getElementsByTagName("input")[0].value);
+	let number2=parseFloat(document.getElementsByTagName("input")[1].value);
+	let number3=parseFloat(number1/number2);
 	(number3);
 	document.getElementsByTagName("span")[0].innerHTML=number3;
 }
